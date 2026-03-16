97be69e9955695be22f07a9e0cee6f2df05049ca
ypatil45349 <ypatil45349@gmail.com>
2026-03-16 16:12:15 +0530

    fix vector db directory

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,17 +1,18 @@
 from langchain_community.vectorstores import Chroma
 from langchain_community.embeddings import HuggingFaceEmbeddings
+import os
+
+os.makedirs("data/vector_db", exist_ok=True)
 
 embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2")
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
 
-# Create vector database
 vector_db = Chroma(
     persist_directory="data/vector_db",
     embedding_function=embeddings
 )
 
 def store_chunks(chunks):
-
     vector_db.add_texts(chunks)
-
     vector_db.persist()
