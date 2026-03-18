d6409676857a89fa342a17fc7b84caf108495dd8
ypatil45349 <ypatil45349@gmail.com>
2026-03-18 19:05:10 +0530

    fix vector store initialization

--- app/services/vector_store.py
+++ app/services/vector_store.py
@@ -1,29 +1,31 @@
 import os
+from langchain_community.vectorstores import Chroma
+from langchain_community.embeddings import HuggingFaceEmbeddings
 
+# Ensure vector DB folder exists
 os.makedirs("data/vector_db", exist_ok=True)
 
-vector_db = None
+# Load embedding model once
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+# Create vector database
+vector_db = Chroma(
+    persist_directory="data/vector_db",
+    embedding_function=embeddings
+)
 
 
 def get_vector_db():
-    global vector_db
-
-    if vector_db is None:
-        from langchain_community.vectorstores import Chroma
-        from langchain_community.embeddings import HuggingFaceEmbeddings
-
-        embeddings = HuggingFaceEmbeddings(
-    model_name="sentence-transformers/all-MiniLM-L6-v2")
-
-        vector_db = Chroma(
-            persist_directory="data/vector_db",
-            embedding_function=embeddings
-        )
-
     return vector_db
 
 
 def store_chunks(chunks):
     db = get_vector_db()
+
     db.add_texts(texts=chunks)
+
     db.persist()
+
+    return {"message": "Chunks stored successfully"}
